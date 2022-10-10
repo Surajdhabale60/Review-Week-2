09bad999fe20df4ffe8b816052fb3c728803538f
Surajdhabale60 <112876188+Surajdhabale60@users.noreply.github.com>
2022-10-10 21:14:50 +0530

    To Check Whether Number Is Perfect Number Or Not

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ReviewWeek2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number, sum=0,n;
+            Console.WriteLine("enter the number");
+            number = int.Parse(Console.ReadLine());
+            n = number;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+                if (sum == n)
+                {
+                    Console.WriteLine("Enter number is a perfect number");
+                    Console.ReadLine();
+                }
+
+                else
+                {
+                    Console.WriteLine("Enter number is not a perfect number");
+                    Console.ReadLine();
+                }
+        }
+    }
+}
